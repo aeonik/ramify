58175c1c1778eefffb3434f85cdd4ea750187b10
Aeonik Chaos <aeonik.chaos@gmail.com>
2023-04-07 15:03:13 -0400

    Added delete sync to disk in ramify

--- ramify.sh
+++ ramify.sh
@@ -1,86 +1,69 @@
 #!/bin/bash
 
-# $1 is the location of the ramdisk
-# $2 is the location to be ramified 
-
-USER_SIZE=0
-
-# Get optional arguments
+# Parsing optional arguments
 while getopts ":s:" OPTIONS; do
-        case $OPTIONS in
-
-                s)
-			echo $OPTARG
-                        USER_SIZE=$OPTARG
-                        ;;
-
-	        *)
-            		echo "Invalid option: -$OPTARG" >&2
-            		usage
-            		exit 2
-            		;;
-        esac
+    case $OPTIONS in
+        s)
+            echo "User size: $OPTARG"
+            USER_SIZE=$OPTARG
+            ;;
+        *)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 2
+            ;;
+    esac
 done
 
-# Shift Optional variables to allow for the rest of the arguments to pass
+# Shift the optional arguments
 shift $((OPTIND - 1))
 
+# Setting variables for target and shadow paths, and owner/group
 TARGET_PATH="${1}"
 SHADOW_PATH="${1}.ramify"
 USER=$(stat -c '%U' "${TARGET_PATH}")
 GROUP=$(stat -c '%G' "${TARGET_PATH}")
 
-echo "User defined size: $USER_SIZE"
+# Cleanup function to handle script termination
 function cleanup() {
-	echo "***CLEANUP CALLED***"
-	rsync -avz "${TARGET_PATH}"/ "${SHADOW_PATH}"/
-	umount "${TARGET_PATH}"
-	rmdir "${TARGET_PATH}"
-	mv "${SHADOW_PATH}" "${TARGET_PATH}"
-	exit
+    echo "***CLEANUP CALLED***"
+    rsync -av --delete "${TARGET_PATH}"/ "${SHADOW_PATH}"/
+    umount "${TARGET_PATH}"
+    rmdir "${TARGET_PATH}"
+    mv "${SHADOW_PATH}" "${TARGET_PATH}"
+    exit
 }
 
-# Check for open files on the system.
-# TODO add logic to prompt user to terminate processes, or pause execution
-echo "Number of open files: $(lsof "${TARGET_PATH}" 2> /dev/null | wc -l)"
-
-# Check folder size
+# Calculate folder size and RAM disk size
 SIZE=$(du -s "${TARGET_PATH}" | cut -f 1)
-echo "Size of target: ${SIZE}KB"
-
-# Partition tmpfs with 10% increase size.
-# Removed AWK because AWK has a hard time with floating points
-# RAM_SIZE=$(awk "BEGIN {print ($SIZE+(0.1 * $SIZE))}")
 RAM_SIZE=$(python -c 'import sys; size=int(sys.argv[1]); print(int(size+(0.1*size)));' "$SIZE")
-# Set size manually if user supplied option
+
+# Use user-defined size if provided
 if [ $USER_SIZE -gt 0 ]; then
-	echo "User supplied size as: ${USER_SIZE}KB"
-	RAM_SIZE=$USER_SIZE
+    RAM_SIZE=$USER_SIZE
 fi
 
-echo "Partioning ${RAM_SIZE}KB"
-
-# Move target folder to shadow location
-echo "Moving "${TARGET_PATH}" to "${SHADOW_PATH}""
+# Move target folder to shadow folder and set trap for cleanup
 mv "${TARGET_PATH}" "${SHADOW_PATH}"
-
-# Catch any signals and cleanup
 trap cleanup SIGHUP SIGINT SIGTERM
 
-# Mount tmpfs in the original target location
-echo "Mounting tmpfs on "${TARGET_PATH}""
-echo "mount -t tmpfs -o size=${RAM_SIZE}k tmpfs "${TARGET_PATH}""
+# Create and mount tmpfs at the target location
 mkdir "${TARGET_PATH}"
 mount -t tmpfs -o size=${RAM_SIZE}k tmpfs "${TARGET_PATH}"
 chown $USER:$GROUP "${TARGET_PATH}"
 
-sleep 5s
+# Check for mounting errors
+if [ $? -ne 0 ]; then
+    echo "Failed to mount tmpfs on ${TARGET_PATH}"
+    exit 1
+fi
 
-echo "Copying contents from target to ramdisk..."
+# Rsync the contents of the shadow folder to the RAM disk
 rsync -a --progress "${SHADOW_PATH}"/ "${TARGET_PATH}"/
 
-echo "Engage inotify syncing"
+# Sync changes between the RAM disk and the shadow folder
 while true; do
-  inotifywait -r -e modify,attrib,close_write,move,create,delete "${TARGET_PATH}"
-  rsync -av "${TARGET_PATH}"/ "${SHADOW_PATH}"/
+    inotifywait -r -e modify,attrib,close_write,move,create,delete "${TARGET_PATH}"
+    rsync -av --delete "${TARGET_PATH}"/ "${SHADOW_PATH}"/
+    sleep 300s
+    rsync -av --delete "${TARGET_PATH}"/ "${SHADOW_PATH}"/
 done
